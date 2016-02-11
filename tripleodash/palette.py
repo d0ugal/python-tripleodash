482098c0affb094341a8278b7dd6313cdf22c18b
Dougal Matthews <dougal@redhat.com>
2016-02-11 11:20:49 +0000

    Initial commit

---
+++ tripleodash/palette.py
@@ -0,0 +1,10 @@
+palette = [
+    ('body', 'white', 'black'),
+    ('main header', 'black', 'light blue', 'bold'),
+    ('header', 'white', 'dark blue', 'bold'),
+    ('row_a', 'white', 'dark gray', 'bold'),
+    ('row_b', 'white', 'black', 'bold'),
+    ('line', 'white', 'black'),
+    ('button normal', 'white', 'black'),
+    ('button select', 'black', 'dark green'),
+]
