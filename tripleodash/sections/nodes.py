96ef12816a197798f6fac5a82234308979a45f2b
Dougal Matthews <dougal@dougalmatthews.com>
2016-02-17 08:21:42 +0000

    Improve node table

--- tripleodash/sections/nodes.py
+++ tripleodash/sections/nodes.py
@@ -8,13 +8,13 @@ from tripleodash import util
 class NodeRow(urwid.WidgetWrap):
     def __init__(self, uuid, instance_uuid, power_state,
                  provision_state, maintenance, widget=urwid.Text,
-                 selectable=True):
+                 selectable=True, squash_instance=False):
 
         self._selectable = selectable
 
         cols = urwid.Columns([
             (40, widget(str(uuid))),
-            (40, widget(str(instance_uuid))),
+            (40 if not squash_instance else 20, widget(str(instance_uuid))),
             (20, widget(str(power_state))),
             (20, widget(str(provision_state))),
             (20, widget(str(maintenance))),
@@ -36,18 +36,23 @@ class NodesWidget(DashboardWidget):
 
     def widgets(self):
 
-        nodes = [
+        node_table = [
             NodeRow("UUID", "Instance UUID", "Power State", "Provision State",
                     "Maintenance", widget=util.header, selectable=False),
             urwid.Divider(),
         ]
 
-        for i, node in enumerate(clients.ironicclient().node.list()):
+        nodes = list(clients.ironicclient().node.list())
+
+        assigned = False in [node.instance_uuid is None for node in nodes]
+
+        for i, node in enumerate(nodes):
 
             widget = util.row_a if i % 2 else util.row_b
 
-            nodes.append(NodeRow(
+            node_table.append(NodeRow(
                 node.uuid, node.instance_uuid, node.power_state,
-                node.provision_state, node.maintenance, widget=widget))
+                node.provision_state, node.maintenance, widget=widget,
+                squash_instance=not assigned))
 
-        return super(NodesWidget, self).widgets() + nodes
+        return super(NodesWidget, self).widgets() + node_table
