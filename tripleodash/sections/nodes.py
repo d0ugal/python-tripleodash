d98b3cc49a59b75c8168282b7759dcbd0cecb1b7
Dougal Matthews <dougal@dougalmatthews.com>
2016-02-17 08:23:19 +0000

    Further node list improvements

--- tripleodash/sections/nodes.py
+++ tripleodash/sections/nodes.py
@@ -14,7 +14,7 @@ class NodeRow(urwid.WidgetWrap):
 
         cols = urwid.Columns([
             (40, widget(str(uuid))),
-            (40 if not squash_instance else 20, widget(str(instance_uuid))),
+            (40 if not squash_instance else 10, widget(str(instance_uuid))),
             (20, widget(str(power_state))),
             (20, widget(str(provision_state))),
             (20, widget(str(maintenance))),
@@ -36,15 +36,15 @@ class NodesWidget(DashboardWidget):
 
     def widgets(self):
 
+        nodes = list(clients.ironicclient().node.list())
+        assigned = False in [node.instance_uuid is None for node in nodes]
+
         node_table = [
             NodeRow("UUID", "Instance UUID", "Power State", "Provision State",
-                    "Maintenance", widget=util.header, selectable=False),
+                    "Maintenance", widget=util.header, selectable=False,
+                    squash_instance=not assigned),
             urwid.Divider(),
         ]
-
-        nodes = list(clients.ironicclient().node.list())
-
-        assigned = False in [node.instance_uuid is None for node in nodes]
 
         for i, node in enumerate(nodes):
 
